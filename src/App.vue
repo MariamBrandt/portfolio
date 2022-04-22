d64fb966af3d23607f3c7379407ed3332a10a037
MariamBrandt <94304066+MariamBrandt@users.noreply.github.com>
2022-04-22 11:46:54 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <Navigation-Component />
+  <router-view/>
+  <transition
+  component>  ls
+  </transition>
+  <FooterComponent />
+</template>
+
+<script>
+import NavigationComponent from './components/NavigationComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+ components: {
+   FooterComponent,
+   NavigationComponent
+ }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+
+</style>
+
